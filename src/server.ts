3843b130f3cc579724b48abeb17627e734a2fe4a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-04-22 10:53:21 -0300

    chore: load environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import fastify from 'fastify'
 
+import { ENV } from './env'
+
 const app = fastify()
 
 app
-  .listen({ port: 3333, host: '0.0.0.0' })
+  .listen({ port: ENV.PORT, host: '0.0.0.0' })
   .then((url) => console.log(`HTTP Server Running! url:${url}`))
