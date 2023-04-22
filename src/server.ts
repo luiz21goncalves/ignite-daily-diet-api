d04d318b6b2debb0f5841af834c711025a2902a8
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-04-22 11:26:14 -0300

    feat: add custom error handler

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
 
 import { ENV } from './env'
 import { appRoutes } from './routes'
@@ -6,6 +7,18 @@ import { appRoutes } from './routes'
 const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _request, replay) => {
+  console.log(error)
+
+  if (error instanceof ZodError) {
+    return replay
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  return replay.status(500).send({ message: 'Internal server error' })
+})
 
 app
   .listen({ port: ENV.PORT, host: '0.0.0.0' })
