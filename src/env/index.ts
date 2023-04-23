a33776657d81fdce781e79cb16fba9eaf8872225
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-04-23 11:44:26 -0300

    chore: add logger environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,16 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  LOGGER_ENABLED: z.coerce.boolean(),
+  LOGGER_LEVEL: z.enum([
+    'trace',
+    'debug',
+    'info',
+    'warn',
+    'error',
+    'fatal',
+    'silent',
+  ]),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,16 +1,20 @@
 import pino, { LoggerOptions } from 'pino'
 
 import { isDev } from '@/constants'
+import { ENV } from '@/env'
 
-const config: LoggerOptions = isDev
-  ? {
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-        },
-      },
-    }
-  : {}
+const config: LoggerOptions = {
+  level: ENV.LOGGER_LEVEL,
+  enabled: ENV.LOGGER_ENABLED,
+}
+
+if (isDev) {
+  config.transport = {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  }
+}
 
 export const logger = pino(config)
